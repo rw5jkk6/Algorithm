57977ad0fcd25a4dc6654ffcacdec3304a891d6d
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-06-14 16:25:51 +0900

    Create buble_sort.jl

---
+++ bubleSort/buble_sort.jl
@@ -0,0 +1,18 @@
+errorになる
+
+# list = [4, 1, 3, 2]
+
+# function swap(a, b)
+#     tmp = a
+#     a = b
+#     b = tmp
+# end
+
+# for i = 1:5
+#     for j = 1:5
+#         if list[j] > list[j+1]
+#             swap(list[j], list[j+1])
+#         end
+#     end
+# end
+# print(list)
