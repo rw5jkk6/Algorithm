12a1fca8336d2c17fb277b13f18278346c3f217f
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-11-13 08:06:02 +0900

    Delete sum-max directory

--- sum-max/max/python.py
+++
@@ -1,8 +0,0 @@
-def max(num_list):
-    maxnum = 0 
-    for i in num_list:
-        if maxnum < i:
-           maxnum = i
-    return maxnum
-
-print(max([i for i in range(10000000)]))
--- sum-max/sum/python.py
+++
@@ -1,7 +0,0 @@
-def sum(num_list):
-    sum = 0
-    for i in num_list:
-        sum += i
-    return sum
-
-print(sum([i for i in range(100000000)]))
