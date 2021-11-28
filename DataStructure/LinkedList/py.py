a4d6195f26fb134615e609fb5c5d92c8457568ae
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-11-28 22:42:26 +0900

    Create py.py

---
+++ DataStructure/LinkedList/py.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return 
+        
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+if __name__ == '__main__':
+    l = LinkedList()
+    l.append(1)
+    l.append(2)
+    l.append(3)
+    print(l.head.next.data) # 2
